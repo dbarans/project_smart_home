2a95d39330e501e445b562a45c6fd705df8044bc
Dominik Baranski <baranskidominik109@gmail.com>
2022-05-03 17:41:15 +0200

    utworzenie funkcji collect_diagnostics

---
+++ iot/diagnostics.py
@@ -0,0 +1,6 @@
+from devices import SmartSpeaker, HueLight, Curtains
+
+
+def collect_diagnostics(device) -> None:
+    print("Connecting to diagnostics server.")
+    print(f"Sending status update {device.status_update()} to server.")
