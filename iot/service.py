9552343b7bb94d6f24bc4a6ab2bfc8da1bef4d98
Dominik Baranski <baranskidominik109@gmail.com>
2022-05-03 21:05:34 +0200

    utworzenie klasy IOTService

--- iot/service.py
+++ iot/service.py
@@ -1,5 +1,8 @@
-import string
 import random
+import string
+from iot.diagnostics import collect_diagnostics
+
+# from devices import *
 
 
 def generate_id(length):
@@ -9,3 +12,35 @@ def generate_id(length):
         letter = random.choice(alphabet)
         device_id += letter
     return device_id
+
+
+class IOTService:
+
+    def __init__(self):
+        self.devices = []
+
+    def register_device(self, device):
+        device_id = generate_id(30)
+        device_object = device(device_id)
+        self.devices.append(device_object)
+        device_object.connect()
+
+    def unregister_device(self, device_id):
+        for object in self.devices:
+            if object.device_id == device_id:
+                object.disconnect()
+                self.devices.remove(object)
+                break
+
+    def get_device(self, device_id):
+        for object in self.devices:
+            if object.device_id == device_id:
+                return object
+
+    def run_program(self, message: list):
+        print("=====RUNNING PROGRAM======")
+
+    def test_devices(self):
+        print("Start test devices")
+        for device in self.devices:
+            collect_diagnostics(device)
