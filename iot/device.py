bab2ab7f74cf6ed8d7cb7b2af8bf7b00937baf6b
Dominik Baranski <baranskidominik109@gmail.com>
2022-05-03 17:11:16 +0200

    utworzenie klasy Device

---
+++ iot/device.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from message import MessageType
+
+
+class Device(ABC):
+    @abstractmethod
+    def connect(self) -> None: pass
+
+    @abstractmethod
+    def disconnect(self) -> None: pass
+
+    @abstractmethod
+    def send_message(self, message_type: MessageType, data: str): pass
+
+    @abstractmethod
+    def status_update(self) -> str: pass
